2db86df08693af0d0bbed977dc83914641f53f58
Akshatha B <akshatha1410@gmail.com>
2026-08-14 05:48:25 +0530

    useState object manually merging  using spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import NormalCounter from "./components/useState/NormalCounter";
 import CounterTwo from "./components/useState/CounterTwo";
+import CounterObject from "./components/useState/CounterObject";
 
 function App() {
   return (
     <>
       {/* <NormalCounter /> */}
-      <CounterTwo />
+      {/* <CounterTwo /> */}
+      <CounterObject />
     </>
   );
 }
---
+++ src/components/useState/CounterObject.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const CounterObject = () => {
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+  return (
+    <>
+      <input
+        value={name.firstName}
+        onChange={(e) => setName({ ...name, firstName: e.target.value })}
+      />
+      <input
+        value={name.lastName}
+        onChange={(e) => setName({ ...name, lastName: e.target.value })}
+      />
+      <h2>First name is :{name.firstName}</h2>
+      <h2>Last name is :{name.lastName}</h2>
+      <h2>{JSON.stringify(name)}</h2>
+    </>
+  );
+};
+
+export default CounterObject;
+
+problem: if i type firstname lastname getting dissapeared ,if i type lastname firstname will dissapeared,coz usestate automatically doesnot merge and update object ,so we should spread operator
