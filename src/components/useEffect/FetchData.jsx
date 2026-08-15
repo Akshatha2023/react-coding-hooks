19a28988246c90c95fac67a0ce4f489e3b6c2a57
Akshatha B <akshatha1410@gmail.com>
2026-08-16 03:05:28 +0530

    we are fetching data by id

--- src/components/useEffect/FetchData.jsx
+++ src/components/useEffect/FetchData.jsx
@@ -1,12 +1,43 @@
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const FetchData = () => {
+//   const [posts, setPosts] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("https://jsonplaceholder.typicode.com/posts")
+//       .then((res) => {
+//         console.log(res.data);
+//         setPosts(res.data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, []);
+//   return (
+//     <>
+//       <ol>
+//         {posts.map((post) => (
+//           <li key={post.id}>{post.title}</li>
+//         ))}
+//       </ol>
+//     </>
+//   );
+// };
+
+// export default FetchData;
+// Above code is  Normal data fetching using useEffect and returning the data and defining [] to make sure api call should happen only once
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 const FetchData = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState({});
+  const [id, setId] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
         console.log(res.data);
         setPosts(res.data);
@@ -14,18 +45,16 @@ const FetchData = () => {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [id]);
   return (
     <>
+      <input type="number" onChange={(e) => setId(e.target.value)} />
       <ol>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
+        <li key={posts.id}>{posts.title}</li>
       </ol>
     </>
   );
 };
 
 export default FetchData;
-
-// Normal data fetching using useEffect and returning the data and defining [] to make sure api call should happen only once
+// above code -Here we are fetching data by id
