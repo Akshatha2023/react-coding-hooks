962db4a8a985286f4419032872cedd5d4aa306a2
Akshatha B <akshatha1410@gmail.com>
2026-08-16 03:07:40 +0530

    setted value to id by button Click

--- src/components/useEffect/FetchData.jsx
+++ src/components/useEffect/FetchData.jsx
@@ -28,16 +28,52 @@
 
 // export default FetchData;
 // Above code is  Normal data fetching using useEffect and returning the data and defining [] to make sure api call should happen only once
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const FetchData = () => {
+//   const [posts, setPosts] = useState({});
+//   const [id, setId] = useState(1);
+
+//   useEffect(() => {
+//     axios
+//       .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+//       .then((res) => {
+//         console.log(res.data);
+//         setPosts(res.data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, [id]);
+//   return (
+//     <>
+//       <input type="number" onChange={(e) => setId(e.target.value)} />
+//       <ol>
+//         <li key={posts.id}>{posts.title}</li>
+//       </ol>
+//     </>
+//   );
+// };
+
+// export default FetchData;
+// above code -Here we are fetching data by id
+
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 const FetchData = () => {
   const [posts, setPosts] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         console.log(res.data);
         setPosts(res.data);
@@ -45,10 +81,11 @@ const FetchData = () => {
       .catch((err) => {
         console.log(err);
       });
-  }, [id]);
+  }, [idFromButtonClick]);
   return (
     <>
       <input type="number" onChange={(e) => setId(e.target.value)} />
+      <button onClick={handleClick}>Fetch data</button>
       <ol>
         <li key={posts.id}>{posts.title}</li>
       </ol>
@@ -57,4 +94,4 @@ const FetchData = () => {
 };
 
 export default FetchData;
-// above code -Here we are fetching data by id
+//In this above code we set value to id by button click ,and only when we click on that button we can fetch data
